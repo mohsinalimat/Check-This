2a6d7a382e30f57d0370e64f228a314ac99e48f4
luismgb <luismigelgb@gmail.com>
2018-07-11 00:13:35 -0700

    Keep keyboard until user clicks 'cancel' on SearchBar

--- Do This/Controllers/ItemViewController+SearchBar.swift
+++ Do This/Controllers/ItemViewController+SearchBar.swift
@@ -15,13 +15,15 @@ extension ItemViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchText.isEmpty {
             loadItems()
-            DispatchQueue.main.async {
-                searchBar.resignFirstResponder()
-            }
         } else {
             items = selectedCategory?.items.filter("name CONTAINS[cd] %@", searchBar.text!)
             tableView.reloadData()
         }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.becomeFirstResponder()
+        print("\nsearchBarSearchButtonClicked called")
     }
     
 }
