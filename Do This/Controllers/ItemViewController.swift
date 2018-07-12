e731543fc5b990da0a647ee22a48e54d2c89fae6
luismgb <luismigelgb@gmail.com>
2018-07-12 12:42:49 -0700

    Fix disappearing emojis bug with strikethrough

--- Do This/Controllers/ItemViewController.swift
+++ Do This/Controllers/ItemViewController.swift
@@ -48,7 +48,7 @@ class ItemViewController: SwipeTableViewController {
         guard let item = items?[indexPath.row] else { fatalError() }
         let cellTitle = NSMutableAttributedString(string: item.name)
         if item.done {
-            cellTitle.addAttribute(NSAttributedStringKey.strikethroughStyle, value: 1, range: NSRange(location: 0, length: item.name.count))
+            cellTitle.addAttribute(NSAttributedStringKey.strikethroughStyle, value: 1, range: NSRange(location: 0, length: cellTitle.length))
             cell.imageView?.image = UIImage(named: "Checked_Checkbox_Icon")
         } else {
             cell.imageView?.image = UIImage(named: "Unchecked_Checkbox_Icon")
