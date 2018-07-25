d150ff8f251836fd06acff247ccae9a9b6acf91e
luismgb <luismigelgb@gmail.com>
2018-07-25 12:39:31 -0700

    Navigate back if item is swiped from the left

--- Do This/Controllers/CustomTableVC/CustomTableVC+Swipe.swift
+++ Do This/Controllers/CustomTableVC/CustomTableVC+Swipe.swift
@@ -15,27 +15,33 @@ extension CustomTableVC: SwipeTableViewCellDelegate {
     func tableView(_ tableView: UITableView,
                    editActionsForRowAt indexPath: IndexPath,
                    for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
-        guard orientation == .right else { return nil }
-        var actions = [SwipeAction]()
-        let swipeToEditDescription: String? = self is CategoryVC ? "Edit" : nil
-        let swipeToDeleteDescription: String? = self is CategoryVC ? "Delete" : nil
         
-        let editAction = SwipeAction(style: .default, title: swipeToEditDescription) { _, indexPath in
-            tableView.cellForRow(at: indexPath)?.selectionStyle = .none
-            tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
-            self.presentEditAlertController(at: indexPath)
+        switch orientation {
+        case .right:
+            var actions = [SwipeAction]()
+            let swipeToEditDescription: String? = self is CategoryVC ? "Edit" : nil
+            let swipeToDeleteDescription: String? = self is CategoryVC ? "Delete" : nil
+            
+            let editAction = SwipeAction(style: .default, title: swipeToEditDescription) { _, indexPath in
+                tableView.cellForRow(at: indexPath)?.selectionStyle = .none
+                tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
+                self.presentEditAlertController(at: indexPath)
+            }
+            let deleteAction = SwipeAction(style: .destructive, title: swipeToDeleteDescription) { _, indexPath in
+                self.delegate.deleteElement(at: indexPath)
+                self.delegate.setTableViewAppearance()
+            }
+            
+            deleteAction.image = UIImage(named: "Delete_Icon")
+            editAction.image = UIImage(named: "More_Icon")
+            actions.append(deleteAction)
+            actions.append(editAction)
+            return actions
+            
+        case .left:
+            navigationController?.popViewController(animated: true)
+            return nil
         }
-        let deleteAction = SwipeAction(style: .destructive, title: swipeToDeleteDescription) { _, indexPath in
-            self.delegate.deleteElement(at: indexPath)
-            self.delegate.setTableViewAppearance()
-        }
-
-        deleteAction.image = UIImage(named: "Delete_Icon")
-        editAction.image = UIImage(named: "More_Icon")
-        actions.append(deleteAction)
-        actions.append(editAction)
-
-        return actions
     }
 
     func tableView(_ tableView: UITableView,
