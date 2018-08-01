a869fcd0201b9171df9fddf6dcff77bc305b5878
luismgb <luismigelgb@gmail.com>
2018-07-31 18:20:48 -0700

    Remove unnecessary comments

--- Check This/Controllers/ItemVC/ItemVC+CustomTable.swift
+++ Check This/Controllers/ItemVC/ItemVC+CustomTable.swift
@@ -18,32 +18,11 @@ extension ItemVC: CustomTableDelegate {
     }
     
     func deleteElement(at indexPath: IndexPath) {
-//        print("\nDELETEELEMENT indexPath: \(indexPath)")
-//        let selectedCell = tableView.cellForRow(at: indexPath)
-//        var persistedIndexRow = Int()
-//        print("items?.count: \(String(describing: items?.count))")
-//        for item in items! {
-//            if item.name == selectedCell?.textLabel?.text { // swiftlint:disable:this for_where superfluous_disable_command
-//                persistedIndexRow = item.persistedIndexRow
-//                print("DELETEELEMENT IF IF IF persistedIndexRow: \(persistedIndexRow)")
-//                return
-//            }
-////            else {
-////                persistedIndexRow = selectedCategory!.items[indexPath.row].persistedIndexRow
-////                print("DELETEELEMENT ELSE persistedIndexRow: \(persistedIndexRow)")
-////            }
-//        }
-//
-//        let persistedIndex = IndexPath(row: persistedIndexRow, section: 0)
-//        deleteItem(at: persistedIndex)
         deleteItem(at: indexPath)
     }
     
-    /// Resets Indexes only if user is not performing a search.
     func resetIndexes() {
-        if presentedViewController == nil {
-            resetItemsPersistedIndexRow()
-        }
+        resetItemsPersistedIndexRow()
     }
     
     func setTableViewAppearance() {
